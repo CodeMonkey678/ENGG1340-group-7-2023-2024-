83149ddcbc32867839c3e4c2a7e786750b53e6bc
Tse Wang Pok Jerry 3036065642 <126223772+JerryTseee@users.noreply.github.com>
2023-11-13 20:31:51 +0800

    Update and rename game.cpp to Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+using namespace std;
+//import three different level mazes in the game.
+#include "Simple_Level_Maze.h" 
+#include "Middle_Level_Maze.h"
+#include "High_Level_Maze.h"
+
+int main()
+{
+  
+	cout << " __  __       __		 ________    ________			" << endl;
+	cout << "| \\/  |     / \\      |______//   | _______|			" << endl;
+	cout << "| \\/  |    /___\\           //    ||_______			" << endl;
+	cout << "| \\/| |   / ___\\\\        //      | _______|			" << endl;
+	cout << "| | | |  / /    \\\\      //_____  ||_______			" << endl;
+	cout << "|_| |_| /_/     \\_\\    //______| |________|         " << endl;
+	cout << "                                                       " << endl;
+	cout << "             Welcome to the Game!                      " << endl;
+
+  
+
+  return 0;
+}
