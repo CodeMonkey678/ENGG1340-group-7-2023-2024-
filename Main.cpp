2e17d592bca002fffb19b0379f9f389bddbed680
Tse Wang Pok Jerry 3036065642 <126223772+JerryTseee@users.noreply.github.com>
2023-11-14 19:36:55 +0800

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -6,14 +6,6 @@ using namespace std;
 int main()
 {
   
-	cout << " __  __       __		 ________    ________			" << endl;
-	cout << "| \\/  |     / \\      |______//   | _______|			" << endl;
-	cout << "| \\/  |    /___\\           //    ||_______			" << endl;
-	cout << "| \\/| |   / ___\\\\        //      | _______|			" << endl;
-	cout << "| | | |  / /    \\\\      //_____  ||_______			" << endl;
-	cout << "|_| |_| /_/     \\_\\    //______| |________|         " << endl;
-	cout << "                                                       " << endl;
-	cout << "             Welcome to the Game!                      " << endl;
 	
 	//this is a simple maze
   	char aa[15][12] = 
