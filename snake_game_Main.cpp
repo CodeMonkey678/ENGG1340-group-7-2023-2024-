4de02a4f3c1dd5e5c2763b6913962d467cf19570
Tse Wang Pok Jerry 3036065642 <126223772+JerryTseee@users.noreply.github.com>
2023-11-15 19:22:49 +0800

    Update snake_game_Main.cpp

--- snake_game_Main.cpp
+++ snake_game_Main.cpp
@@ -1,1 +1,22 @@
+#include<ncurses.h> //ncurses is a library for creating text-based user interface. eg. handling user keyboard input
+#include "src/Board.hpp"
 
+int main(int argc, char **argv)
+{
+  initscr();
+  refresh();
+
+  //making the gameboard
+  
+  //get the x and y axis size
+  int x, y;
+  getxy(stdscr,y,x);
+  WINDOW * board_win = newwin(20, 20, (y/2)-10, (x/2)-10);//(height,width, display in the middle)
+  box(board_win,0,0);
+  wrefresh(board_win);//refresh the window
+  
+  getch();
+  endwin();
+
+  return 0;
+}
