73721bf5270046e2a78b99c065e577e83fdc86dc
Tse Wang Pok Jerry 3036065642 <126223772+JerryTseee@users.noreply.github.com>
2023-11-15 20:06:22 +0800

    Create SnakeGame.hpp

---
+++ SnakeGame.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#include <ncurses.h>
+#include "Board.hpp"
+
+class SnakeGame
+{
+  SnakeGame (int height, int width)
+  {
+    
+  }
+  void processInput()
+  {
+    
+  }
+  void updateState()
+  {
+    
+  }
+  void redraw()
+  {
+    
+  }
+  bool isOver()
+  {
+    
+  }
+private:
+  Board board;
+  bool game_over;
+}
