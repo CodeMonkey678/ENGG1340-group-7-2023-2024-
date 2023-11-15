3e5039557f9a1c116a07b9fe1e85ab16e408883a
Tse Wang Pok Jerry 3036065642 <126223772+JerryTseee@users.noreply.github.com>
2023-11-15 20:13:00 +0800

    Update SnakeGame.hpp

--- SnakeGame.hpp
+++ SnakeGame.hpp
@@ -6,11 +6,12 @@ class SnakeGame
 {
   SnakeGame (int height, int width)
   {
-    
+    board = Board(height, width);
+    game_over = false;
   }
   void processInput()
   {
-    
+    chtype input = board.getInput();
   }
   void updateState()
   {
@@ -18,11 +19,11 @@ class SnakeGame
   }
   void redraw()
   {
-    
+    board.refresh();
   }
   bool isOver()
   {
-    
+    return game_over;
   }
 private:
   Board board;
