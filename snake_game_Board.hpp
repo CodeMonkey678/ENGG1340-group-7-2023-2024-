e1a8fb1e7b4babfb71a92dd4cab14dd2b62996c1
Tse Wang Pok Jerry 3036065642 <126223772+JerryTseee@users.noreply.github.com>
2023-11-15 19:54:06 +0800

    Update snake_game_Board.hpp

--- snake_game_Board.hpp
+++ snake_game_Board.hpp
@@ -11,13 +11,36 @@ public:
         getxy(stdscr,yMax,xMax);
             
         board_win = newwin(height, width, (yMax/2)-(height-2), (xMax/2)-(width-2));//(height,width, display in the middle)-->make a square window
-        box(board_win,0,0);
-        wrefresh(board_win);//refresh the window
-            
-        this->height = height;
-        this->width = width;
+        initialize();
       }
+      void initialize()
+      {
+        clear();//first run a clear
+        refresh();
+      }
+      void addBorder()
+      {
+        box(board_win, 0, 0);
+      }
+
+      void addAt(int y, int x, chtype ch)
+      {
+        mvwaddch(board_win, y, x, ch);
+      }
+      chtype getInput()
+      {
+        return wgetch(board_win);
+      }
+      void clear()
+      {
+        wclear(board_win);//clear the whole window
+        addBorder();//create the board again
+      }
+      void refresh()
+      {
+        wrefresh(board_win);
+      }
+
 private:
       WINDOW * board_win;
-      int height, width;
 };
