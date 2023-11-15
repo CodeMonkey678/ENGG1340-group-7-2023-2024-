9069a7ee1c9a5c72a42a8803e44db3f4d07c718f
Tse Wang Pok Jerry 3036065642 <126223772+JerryTseee@users.noreply.github.com>
2023-11-15 19:36:51 +0800

    Update snake_game_Board.hpp

--- snake_game_Board.hpp
+++ snake_game_Board.hpp
@@ -3,9 +3,21 @@
 class Board
 {
 public:
-      Board()
+      Board(int height, int width)
       {
-        
+        //making the gameboard
+        //get the x and y axis size
+        int xMax, yMax;
+        getxy(stdscr,yMax,xMax);
+            
+        board_win = newwin(height, width, (yMax/2)-(height-2), (xMax/2)-(width-2));//(height,width, display in the middle)-->make a square window
+        box(board_win,0,0);
+        wrefresh(board_win);//refresh the window
+            
+        this->height = height;
+        this->width = width;
       }
 private:
+      WINDOW * board_win;
+      int height, width;
 };
