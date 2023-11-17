5f90f56d0ba1934192d8ef00bb24620c3e9a759e
Palden TAMANG 3036106240 <86504610+CodeMonkey678@users.noreply.github.com>
2023-11-17 12:37:34 +0800

    Update SnakeGame_main.cpp
    Added real time input and output to the game using GETCH

--- SnakeGame_main.cpp
+++ SnakeGame_main.cpp
@@ -5,6 +5,76 @@
 #include <string>
 #include <thread>
 #include <chrono>
+#include <termios.h>
+#include <sys/ioctl.h>
+#include <stdio.h>
+#include <unistd.h>
+
+static struct termios old, newer;
+
+//Initialize the terminal input and output settings.
+void initializeTermios(int echo)
+{
+    //Gets the current terminal attributes from the file descriptor 0 (standard input) and stores them in struct old.
+    tcgetattr(0, &old);
+    newer = old;
+    //Sets the ICANON flag in the c_lflag field of newer to 0 (false) by performing a bitwise AND operation with the bitwise NOT of ICANON.
+    newer.c_lflag &= ~ICANON;
+    newer.c_lflag &= echo ? ECHO : ~ECHO;
+    //calls the tcsetattr function to set the terminal attributes for file descriptor 0 to the values in newer.
+    //The TCSANOW flag specifies that the changes should take effect immediately.
+    tcsetattr(0, TCSANOW, &newer);
+}
+
+//Detect if a key has been pressed on the keyboard without blocking the program's execution.
+bool keyboardhit()
+{
+    termios term;
+    //tcgetattr is called to get the current terminal attributes for file descriptor 0
+    tcgetattr(0, &term);
+
+    termios term2 = term;
+    term2.c_lflag &= ~ICANON;
+    tcsetattr(0, TCSANOW, &term2);
+
+    int byteswaiting;
+    //called the ioctl function to determine how many bytes are waiting in the input buffer.
+    ioctl(0, FIONREAD, &byteswaiting);
+
+    tcsetattr(0, TCSANOW, &term);
+
+    return byteswaiting > 0;
+}
+
+
+//Reset the terminal settings to their original values, which were saved by the initializeTermios() function.
+void resetTermios(void)
+{
+    tcsetattr(0, TCSANOW, &old);
+}
+
+
+//Read a single character of input from the keyboard.
+char getch_(int echo)
+{
+    char ch;
+    initializeTermios(echo);
+    ch = getchar();
+    resetTermios();
+    return ch;
+}
+
+//Wrapper function for getch_() -> no echo to screen
+char GETCH(void)
+{
+    return getch_(0);
+}
+
+//Wrapper function for getch_() -> echoes to screen
+char getche(void)
+{
+    return getch_(1);
+}
 
 
 using namespace std;
@@ -99,21 +169,23 @@ public:
 
 
     void Input() {
-        string input;
-        if (cin >> input) {
-            if (input == "w" && dir != DOWN) {
+        if (keyboardhit())
+        {
+            char input = GETCH();
+            
+            if (input == 'w' && dir != DOWN) {
                 dir = UP;
             }
-            else if (input == "s" && dir != UP) {
+            else if (input == 's' && dir != UP) {
                 dir = DOWN;
             }
-            else if (input == "a" && dir != RIGHT) {
+            else if (input == 'a' && dir != RIGHT) {
                 dir = LEFT;
             }
-            else if (input == "d" && dir != LEFT) {
+            else if (input == 'd' && dir != LEFT) {
                 dir = RIGHT;
             }
-            else if (input == "x") {
+            else if (input == 'x') {
                 gameOver = true;
             }
         }
