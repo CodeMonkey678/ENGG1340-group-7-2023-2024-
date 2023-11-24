f6c04ccfacc5b342f5bb7e9f60886199f14844ad
Tse Wang Pok Jerry 3036065642 <126223772+JerryTseee@users.noreply.github.com>
2023-11-24 20:18:15 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,27 +1,12 @@
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
-#include <vector>
-#include <string>
-#include <thread>
-#include <chrono>
-#include <termios.h>
-#include <sys/ioctl.h>
-#include <stdio.h>
-#include <unistd.h>
+#include "SnakeGame.h"
 
-
-
-
-
-
-//this is the main function
 int main() {
-        srand(static_cast<unsigned>(time(nullptr)));
+    srand(static_cast<unsigned>(time(nullptr)));
 
-        SnakeGame game;
-        game.Name();
-        game.Run();
+    SnakeGame game;
+    game.Name();
+    game.Run();
 
-        return 0;
-    }
+    return 0;
+}
