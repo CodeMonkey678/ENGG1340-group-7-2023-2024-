a56470a0ebe4454af9e80c43d0e70e5468adca85
Tse Wang Pok Jerry 3036065642 <126223772+JerryTseee@users.noreply.github.com>
2023-11-24 21:14:37 +0800

    Update InputOutput.h

--- InputOutput.h
+++ InputOutput.h
@@ -2,6 +2,7 @@
 #define INPUTOUTPUT_H
 
 #include <string>
+#include <iostream>
 
 void clearConsole();
 void drawGameBoard(int width, int height, const Position& head, const Position& apple, const std::vector<Position>& snake, int score);
