dac7cd131afb5f83c11463f34e91daecca3e8b14
Tse Wang Pok Jerry 3036065642 <126223772+JerryTseee@users.noreply.github.com>
2023-11-24 21:12:01 +0800

    Create InputOutput.h

---
+++ InputOutput.h
@@ -0,0 +1,11 @@
+#ifndef INPUTOUTPUT_H
+#define INPUTOUTPUT_H
+
+#include <string>
+
+void clearConsole();
+void drawGameBoard(int width, int height, const Position& head, const Position& apple, const std::vector<Position>& snake, int score);
+char getInput();
+void showGameOver();
+
+#endif // INPUTOUTPUT_H
