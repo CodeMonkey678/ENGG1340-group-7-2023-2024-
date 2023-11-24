bd8f225156eddd3ec4e61ac3ecaaabe24626524b
Tse Wang Pok Jerry 3036065642 <126223772+JerryTseee@users.noreply.github.com>
2023-11-24 20:20:14 +0800

    Create SnakeGame.cpp

---
+++ SnakeGame.cpp
@@ -0,0 +1,63 @@
+#include "SnakeGame.h"
+#include <cstdlib>
+#include <ctime>
+#include <thread>
+#include <chrono>
+#include <termios.h>
+#include <sys/ioctl.h>
+#include <stdio.h>
+#include <unistd.h>
+
+static struct termios old, newer;
+
+void initializeTermios(int echo) {
+    // implementation
+}
+
+bool keyboardhit() {
+    // implementation
+}
+
+void resetTermios(void) {
+    // implementation
+}
+
+char getch_(int echo) {
+    // implementation
+}
+
+char GETCH(void) {
+    // implementation
+}
+
+char getche(void) {
+    // implementation
+}
+
+SnakeGame::SnakeGame() {
+    // implementation
+}
+
+Position SnakeGame::GenerateApple() {
+    // implementation
+}
+
+void SnakeGame::Draw() {
+    // implementation
+}
+
+void SnakeGame::Input() {
+    // implementation
+}
+
+void SnakeGame::Logic() {
+    // implementation
+}
+
+void SnakeGame::Run() {
+    // implementation
+}
+
+void SnakeGame::Name() {
+    // implementation
+}
