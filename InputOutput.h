753453f455ccf8adf713d8b25af6f05dd69eee00
Tse Wang Pok Jerry 3036065642 <126223772+JerryTseee@users.noreply.github.com>
2023-11-27 15:39:21 +0800

    Update InputOutput.h

--- InputOutput.h
+++ InputOutput.h
@@ -7,10 +7,10 @@
 #include "SnakeGame.h"
 //this is just a  output head file
 
-void Console_Clear();
-
+void Console_Clear();//this function responsible for cleaning the screen
+//this function is to draw the game board
 void Game_Board(int width, int height, const Position& head, const Position& apple, const std::vector<Position>& snake, int *score);
-char Show_Input();
-void Show_Game_Over();
+char Show_Input();//get the user input's character
+void Show_Game_Over();//show the game is over laaa!1
 
 #endif // INPUTOUTPUT_H
