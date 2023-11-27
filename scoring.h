d8962c279b7485cd814ec8ae5a53601f05e69ace
Abraham Chandafa <abrahamchandafa@gmail.com>
2023-11-28 00:04:53 +0800

    Create scoring.h

---
+++ scoring.h
@@ -0,0 +1,13 @@
+#ifndef SCORING_H
+#define SCORING_H
+
+#include <map>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <istream>
+
+// Function declaration for the scorer function
+void scorer(std::string name, int score);
+
+#endif // SCORING_H
