05ef6905683ed2186b4b89279cf1f6c9bf10d245
Abraham Chandafa <abrahamchandafa@gmail.com>
2023-11-28 12:21:55 +0800

    Create levels.h

---
+++ levels.h
@@ -0,0 +1,12 @@
+#ifndef LEVELS_H
+#define LEVELS_H
+
+#include <map>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <sstream>
+
+int levels(std::string name);
+
+#endif // LEVELS_H
