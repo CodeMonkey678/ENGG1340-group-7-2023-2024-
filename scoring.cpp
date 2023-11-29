c09477c4d29cadea2ef2db287018e332617f357c
Abraham Chandafa <abrahamchandafa@gmail.com>
2023-11-29 11:54:24 +0800

    Update scoring.cpp

--- scoring.cpp
+++ scoring.cpp
@@ -6,11 +6,16 @@
 #include <sstream>
 using namespace std;
 
+/* 
+    -function takes name and new score after game over, returns nothing
+    - handles data storing with user needs and commands, thus actively interacting with user
+*/
+
 void scorer(string name, int score){
-    map<string,int> mapping;
+    map<string,int> mapping; // storing all data efficiently before printing to user & storing to file
 
     ofstream Board;
-    Board.open("scoreBoard.txt", ios::app);
+    Board.open("scoreBoard.txt", ios::app); // useful for first-time running
 
     if(Board.fail()){
         cout << "File open error... please restart program" << endl;
@@ -18,7 +23,7 @@ void scorer(string name, int score){
     }
     Board.close();  // will work for first-time creating a file
 
-    ifstream writer;
+    ifstream writer; 
     writer.open("scoreBoard.txt");
     if(writer.fail()){
         cout << "File open error... please restart program" << endl;
@@ -26,7 +31,7 @@ void scorer(string name, int score){
     }
     string line, word;
     int nameScore;
-    bool failed = true;
+    
     while(getline(writer, line)){
         //wanna treat 'line' as a new input, and traverse through it
         istringstream iss(line);
@@ -41,21 +46,21 @@ void scorer(string name, int score){
     }
     writer.close();
 
-    //now this part is called whenever the player writes his name;
-    //Board.open("scoreBoard.txt", ios::app);
+    //below, if user name exists, h/she is asked if wants to overwrite previous scores
     if(mapping.count(name)>0){
         cout << "\nName already exists... \n";
         cout << "\n Previous score: " << name << " - " << mapping[name] << endl;
         cout << "\noverwrite with new score("<<score<<")? [Y/n]: ";
         string overwrite;
         cin >> overwrite;
+        
         if (overwrite=="Y" || overwrite == "y"){
             //nameScore = score; // Update nameScore variable
             mapping[name] = score; // Update mapping[name] with nameScore
             cout << "\nOverwritten!" << endl;
         }
     } else {
-        mapping[name] = score;
+        mapping[name] = score; //username is new, so new score and name recorded
     }
 
     //now printing the entire standing
@@ -63,8 +68,8 @@ void scorer(string name, int score){
     Board.open("scoreBoard.txt");
     cout << "\n" << "\nSTATS:\n";
     for(itr=mapping.begin(); itr != mapping.end(); itr++){
-        Board << (*itr).first << "  " << (*itr).second << "\n";
-        cout << (*itr).first << " - " << (*itr).second << endl;
+        Board << (*itr).first << "  " << (*itr).second << "\n"; //writing to file
+        cout << (*itr).first << " - " << (*itr).second << endl; //printing for user to see
         
         
     }
@@ -73,7 +78,7 @@ void scorer(string name, int score){
     Board.close();
     string reset;
 
-    cout << "\nWOULD YOU LIKE TO ERASE ALL STATS DATA? [Y/n]: ";
+    cout << "\nWOULD YOU LIKE TO ERASE ALL STATS DATA? [Y/n]: "; //asks whether restart the stats
     cin >> reset;
     if (reset=="Y" || reset == "y"){
             Board.open("scoreBoard.txt");
